70fd01ce8a2a945638044a54859617a098b3b742
tpenaloza <tompenaloza@gmail.com>
2022-03-11 23:38:09 -0400

    Fixed the base url

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://github.com/tpenaloza/manipulacionDOM-FETCH',
+    baseUrl: 'https://github.com/tpenaloza/manipulacionDOM-FETCH/tree/gh-pages',
   },
 }
